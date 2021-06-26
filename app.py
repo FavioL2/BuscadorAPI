c6e2bd642b30fa71f9601356ce758deb6edaa335
Faaaaavio <jfd.serrano17@info.uas.edu.mx>
2021-06-26 02:53:28 -0600

    asdasd

--- app.py
+++ app.py
@@ -48,7 +48,9 @@ def home():
             ruta="https://www.coppel.com"
             idPrecio="price";idNombre="main_header"    
         try:
-            precio = ((html.find(tagPrecio, {entradaPrecio: idPrecio}).text).split())
+            precio = ((html.find(tagPrecio, {entradaPrecio: idPrecio})))
+            if precio is None:
+                precio = (precio.text).split()
             nombre = (html.find(tagNombre, {entradasNombre: idNombre}).text)
             return {
                 "url":url,
